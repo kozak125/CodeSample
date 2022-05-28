a7f13778a6039c322c3ec7789abfb6c7c8ddf388
Janusz Kozakiewicz <xiaolin125@gazeta.pl>
2022-05-28 13:33:56 +0200

    Setup game scene

---
+++ VR_shooting/Assets/Scripts/Enemy.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public void ChangeColorRed()
+    {
+        GetComponent<Renderer>().material.color = Color.red;
+    }
+
+    public void ChangeColorBlue()
+    {
+        GetComponent<Renderer>().material.color = Color.blue;
+    }
+}
